cb4b7d25082d8d0ec90e0ddbc83b4e3de2a1f49f
WashFall <carl.christensen98@gmail.com>
2022-01-20 17:39:15 +0100

    Package update

--- Assets/_Scripts/CardTouch.cs
+++ Assets/_Scripts/CardTouch.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class CardTouch : MonoBehaviour
 {
-    void Update()
-    {
-        if(Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-            touchPosition.z = 0;
-            transform.position = touchPosition;
-        }
-    }
+    //void Update()
+    //{
+    //    if(Input.touchCount > 0)
+    //    {
+    //        Touch touch = Input.GetTouch(0);
+    //        Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+    //        touchPosition.z = 0;
+    //        transform.position = touchPosition;
+    //    }
+    //}
 }
