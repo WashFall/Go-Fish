537db5d82f6c4f3d97705638f5c8bad27ff9cc39
WashFall <carl.christensen98@gmail.com>
2022-02-18 21:35:42 +0100

    Created Login screens

---
+++ Assets/_Scripts/Menus/LoginMenu.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginMenu : MonoBehaviour
+{
+    public void AnonymousPlay()
+    {
+        // TODO: Add anonymous login call
+
+        SceneManager.LoadScene("MainMenu");
+    }
+}
